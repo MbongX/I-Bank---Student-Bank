b395220f85e107cca6a994e9354c44be1e1e93c4
MbongXD <MbongXD@MBONGX-D>
2023-04-12 06:25:11 +0200

    Add project files.

---
+++ I Bank - Student Bank/CustomerRegistration.cs
@@ -0,0 +1,10 @@
+namespace I_Bank___Student_Bank
+{
+    public partial class CustomerRegistration : Form
+    {
+        public CustomerRegistration()
+        {
+            InitializeComponent();
+        }
+    }
+}
